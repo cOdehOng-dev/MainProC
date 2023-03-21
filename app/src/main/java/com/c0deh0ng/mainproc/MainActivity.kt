113b614b374d27e563db919d5b8fe80037391be9
c0de_h0ng <ghdtjrdn0323@gmail.com>
2023-03-21 14:54:27 +0900

    test

--- app/src/main/java/com/c0deh0ng/mainproc/MainActivity.kt
+++ app/src/main/java/com/c0deh0ng/mainproc/MainActivity.kt
@@ -6,31 +6,54 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import java.io.File
+import java.io.FileInputStream
 import java.io.IOException
 import java.io.InputStream
-import java.net.URL
 import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//
+//        val file = File("MainProC/json_info/json_info.json")
 
-        val file = File("json_info/json_info.json")
 
-
-        Log.d("TAG", "test here 1 ${file.name}")
-
-        val ss = getAssetJsonData(this, file.name)
+//
+//        val ss = getAssetJsonData(this, file.name)
 //        val ss = (getJsonFile(this, file.name, JsonDataTest::class.java) as JsonDataTest).version
 //
-        Log.d("TAG", "test here 2 = $ss")
+
+        runCatching {
+//            val filePath = file1.absolutePath
+            val ff = filesDir.path
+            Log.d("TAG", "test here 1 ${ff}")
+            val path: String = getFilesDir().getAbsolutePath()
+            val file1 = File(path + "/json_info.json")
+            val length = file1.length().toInt()
+
+            val bytes = ByteArray(length)
+
+            val `in` = FileInputStream(file1)
+            try {
+                `in`.read(bytes)
+            } finally {
+                `in`.close()
+            }
+
+            val contents = String(bytes)
+            Log.d("TAG", "test here 2 = ${contents}")
+        }.onFailure {
+            Log.d("TAG", "test here error :: ${it}")
+        }
+
+
     }
 
     fun getJsonFile(context: Context?, fileName: String?, dataClass: Class<*>): Any? {
         if (fileName.isNullOrEmpty() || context == null) return null
         return try {
-            val jsonStr =  context.assets.open(fileName).bufferedReader().use { it.readText() }
+            val jsonStr = context.assets.open(fileName).bufferedReader().use { it.readText() }
             Gson().fromJson(jsonStr, dataClass)
         } catch (e: IOException) {
             null
